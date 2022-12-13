c191fff9900e8d3a1bbd92ddfab3e768c4f87924
maksvaliullin <117544522+maksvaliullin@users.noreply.github.com>
2022-12-13 14:26:20 +0300

    Merge remote-tracking branch 'origin/JDBCimpl' into JDBCimpl
    # Conflicts:
    #	src/main/java/jm/task/core/jdbc/util/dao/UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,12 +1,11 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Table(name = "users")
+@Entity
 public class User {
-    //f
+
     @Override
     public String toString() {
         return "User{" +
@@ -18,15 +17,17 @@ public class User {
     }
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "lastname")
     private String lastName;
 
-    @Column
+    @Column(name = "age")
     private Byte age;
 
     public User() {
